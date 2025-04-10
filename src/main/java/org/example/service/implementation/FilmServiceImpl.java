077282c0ef4c20e84ab8481e6af279e12402dbc6
Vitos <bender.carno@gmail.com>
2025-04-10 16:39:20 +0300

    Lab 5 is done, sonar and coverage is good

--- src/main/java/org/example/service/implementation/FilmServiceImpl.java
+++ src/main/java/org/example/service/implementation/FilmServiceImpl.java
@@ -110,7 +110,7 @@ public class FilmServiceImpl implements FilmService {
         }
     }
 
-    @SuppressWarnings("java:S1845")
+    @SuppressWarnings("java:S1854")
     private void saveToCacheAndDb(Film film) {
         Film savedFilm = filmRepository.save(film);
         int size = savedFilm.getActors().size();
