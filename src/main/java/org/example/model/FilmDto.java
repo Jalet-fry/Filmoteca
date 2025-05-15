a19dec6cb27b5ea5050b5795786327ca2533b8f9
Vitos <bender.carno@gmail.com>
2025-05-15 16:56:41 +0300

    Render and BD aaauhj!

--- src/main/java/org/example/model/FilmDto.java
+++ src/main/java/org/example/model/FilmDto.java
@@ -1,6 +1,7 @@
 package org.example.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import java.util.List;
